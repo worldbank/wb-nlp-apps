26a0ed7803f4b6a68f7770b4cf4c92cd3113d7f8
Aivin V. Solatorio <avsolatorio@gmail.com>
2020-07-20 18:24:26 -0400

    Cleanup extractor.

--- src/wb_nlp/extraction/extractor.py
+++ src/wb_nlp/extraction/extractor.py
@@ -6,6 +6,7 @@ from spacy.lang.en import English
 from spacy.matcher import PhraseMatcher
 from spacy.tokens import Doc, Span, Token
 
+from wb_nlp.extraction.whitelist import mappings
 
 class BaseExtractor:
     name = 'base_extractor'
@@ -14,17 +15,16 @@ class BaseExtractor:
         self.label = label
         self.label_id = nlp.vocab.strings[label]
         self.extractor_id = extractor_id
-        self.attribute_id = self.extractor_id.lower()
+        self.type_id = f'is_{extractor_id.lower()}'
         self.mapping = mapping
 
         patterns = [nlp(ent) for ent in entities]
         self.extractor = PhraseMatcher(nlp.vocab)
         self.extractor.add(extractor_id, callback, *patterns)
 
-        Token.set_extension(f'is_{self.attribute_id}', default=False, force=True)
-
-        if self.mapping is not None:
-            Token.set_extension(f'normalized', default='', force=True)
+        Token.set_extension(self.type_id, default=False, force=True)
+        Token.set_extension(f'normalized', default='', force=True)
+        Token.set_extension(f'code', default='', force=True)
 
         # Register and implement attribute and getter in subclasses.
 
@@ -33,12 +33,20 @@ class BaseExtractor:
         spans = []
         with doc.retokenize() as retokenizer:
             for id, start, end in extracted:
-                entity = doc[start:end]  # Span(doc, start, end, label=self.label_id)
-                # spans.append(entity)
+                entity = doc[start:end]
+
+                normed = self.mapping.get(entity.text)
+                if normed is None:
+                    code = ''
+                    normalized = entity.text
+                else:
+                    code = normed.get('code', '')
+                    normalized = normed.get('normalized', entity.text)
 
                 for token in entity:
-                    token._.set('normalized', self.mapping.get(entity.text, entity.text))
-                    token._.set(f'is_{self.attribute_id}', True)
+                    token._.set('normalized', normalized)
+                    token._.set('code', code)
+                    token._.set(self.type_id, True)
                     token.ent_type_ = self.label
 
                 retokenizer.merge(entity)
@@ -49,14 +57,5 @@ class BaseExtractor:
 class CountryExtractor(BaseExtractor):
 
     def __init__(self, nlp, entities: tuple, label: str='COUNTRY', extractor_id: str='COUNTRY'):
-        self.country_mapping = CountryExtractor.load_country_mapping()
+        self.country_mapping = mappings.get_countries_mapping()
         super(CountryExtractor, self).__init__(nlp, entities, label, extractor_id, None, self.country_mapping)
-
-    @staticmethod
-    def load_country_mapping():
-        return {'Philippines': 'Philippines', 'Pilipinas': 'Philippines', 'Sri Lanka': 'Sri Lanka'}
-
-        # # Register attributes on Doc and Span via a getter that checks if one of
-        # # the contained tokens is set to is_tech_org == True.
-        # Doc.set_extension("has_tech_org", getter=self.has_tech_org)
-        # Span.set_extension("has_tech_org", getter=self.has_tech_org)
