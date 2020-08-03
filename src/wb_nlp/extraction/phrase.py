537adf5283249ba0d587563b68f57074f0add775
Aivin V. Solatorio <avsolatorio@gmail.com>
2020-08-03 20:44:22 +0000

    Update phrase extractor with filler.

--- src/wb_nlp/extraction/phrase.py
+++ src/wb_nlp/extraction/phrase.py
@@ -4,6 +4,7 @@ import spacy
 def get_phrases(doc: spacy.tokens.doc.Doc, min_token_length: int=3):
     phrases = []
     curr_phrase = []
+    fillers = ['of', 'the', 'in']  # Not used if
 
     for token in doc:
         # if token.ent_type_:
@@ -20,10 +21,20 @@ def get_phrases(doc: spacy.tokens.doc.Doc, min_token_length: int=3):
             curr_phrase = []
             continue
 
-        if token.pos_ in ['ADJ', 'NOUN']:
+        if len(curr_phrase) > 0 and token.lower_ in fillers:
+            # This will not really work if we only use 'ADJ' and 'NOUN'.
+            # But leaving this here in case we want to capture compound 'PROPN'.
+            curr_phrase.append(token.lemma_)
+        elif token.pos_ in ['ADJ', 'NOUN']:
             curr_phrase.append(token.lemma_ if token.lower_ != 'data' else 'data')
         else:
             if len(curr_phrase) > 1:
+                while True:
+                    if curr_phrase[-1] in fillers:
+                        curr_phrase.pop()
+                    else:
+                        break
+
                 phrases.append('_'.join(curr_phrase))
             curr_phrase = []
 
