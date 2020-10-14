af9be2bd4c47053856db6a25dc8455f0ed30cfe2
Aivin V. Solatorio <avsolatorio@gmail.com>
2020-10-13 20:31:34 -0400

    Make file upload work.

--- app/app_vue2/src/components/pages/Similarity.vue
+++ app/app_vue2/src/components/pages/Similarity.vue
@@ -7,6 +7,38 @@
       <b-row>
         <b-col cols="9" class="border-right">
           Word2vec is a simple embedding model.
+          <FileUpload
+            :url="url"
+            :thumb-url="thumbUrl"
+            :headers="headers"
+            accept=".txt, .pdf"
+            @change="onFileChange"
+          ></FileUpload>
+
+          <div class="input-group mb-3">
+            <div class="custom-file">
+              <form
+                class="form-file-upload"
+                method="post"
+                enctype="multipart/form-data"
+                action="/api/related_docs"
+              >
+                <input
+                  type="file"
+                  ref="file"
+                  name="file"
+                  class="xcustom-file-input"
+                  id="xinputGroupFile01"
+                />
+                <!--<label class="custom-file-label" for="inputGroupFile01">Choose file</label>-->
+                <input
+                  type="submit"
+                  v-on:click="uploadFile"
+                  :disabled="isReady() === false"
+                />
+              </form>
+            </div>
+          </div>
         </b-col>
         <b-col cols="3">
           <h2>Try the API!</h2>
@@ -43,8 +75,14 @@
 </template>
 
 <script>
+import FileUpload from "v-file-upload";
+import $ from "jquery";
+
 export default {
   name: "Similarity",
+  components: {
+    FileUpload,
+  },
   props: {
     page_title: String,
   },
@@ -55,12 +93,194 @@ export default {
       related_words: [],
       raw_text: "",
       loading: true,
+
+      // Model params
+      corpus_id: "WB",
+      word2vec_model_id: "ALL_50",
+      lda_model_id: "ALL_50",
+      // Uploaded file data
+      // fileUploaded: null,
+      is_searching: false,
+      documents: [],
     };
   },
   mounted() {
     this.getRelatedWords();
   },
   methods: {
+    onFileChange: function (file) {
+      // this.fileUploaded = file;
+      window.fup = file;
+      this.postUploadedFile(file);
+    },
+    isReady: function () {
+      return true;
+    },
+    postUploadedFile: function (file) {
+      var data = new FormData();
+      data.append("file", file);
+      window.fd = data;
+      console.log(data);
+
+      this.errors = [];
+      this.uploaded_doc_topics = null;
+      this.is_searching = true;
+
+      var options = {
+        corpus_id: this.corpus_id,
+        model_id: this.word2vec_model_id,
+        topn: 10,
+        clean_doc: true,
+        // data: data,
+      };
+
+      this.$http
+        .post(
+          // "/api/related_docs?corpus_id=WB&model_id=ALL_50&topn=10&clean_doc=true",
+          "/api/related_docs?" + $.param(options),
+          data,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        )
+        .then((response) => {
+          let data = response.data;
+          if ("docs" in data) {
+            this.documents = data.docs;
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          this.errors.push(error);
+          this.errored = true;
+          this.is_searching = false;
+        })
+        .finally(() => {
+          this.is_searching = false;
+        });
+
+      if (this.lda_model_id != "") {
+        options = {
+          corpus_id: this.corpus_id,
+          model_id: this.lda_model_id,
+          topn_topics: 10,
+          total_topic_score: 0.8,
+          clean_doc: true,
+        };
+
+        this.$http
+          .post("/api/lda_doc_topics?" + $.param(options), data, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          })
+          .then((response) => {
+            let data = response.data;
+            if ("topics" in data) {
+              this.uploaded_doc_topics = data.topics;
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+            this.errors.push(error);
+            this.errored = true;
+            this.is_searching = false;
+          })
+          .finally(() => {
+            this.is_searching = false;
+          });
+      }
+    },
+    uploadFile: function (e) {
+      if (!this.isReady()) {
+        return;
+      }
+
+      window.x = this.$refs;
+      var file = this.$refs.file.files[0];
+
+      this.is_file_uploaded = true;
+
+      var data = new FormData();
+      data.append("file", file);
+      window.fd = data;
+      console.log(data);
+
+      this.errors = [];
+      this.uploaded_doc_topics = null;
+      this.is_searching = true;
+
+      var options = {
+        corpus_id: this.corpus_id,
+        model_id: this.word2vec_model_id,
+        topn: 10,
+        clean_doc: true,
+        // data: data,
+      };
+
+      this.$http
+        .post(
+          // "/api/related_docs?corpus_id=WB&model_id=ALL_50&topn=10&clean_doc=true",
+          "/api/related_docs?" + $.param(options),
+          data,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        )
+        .then((response) => {
+          let data = response.data;
+          if ("docs" in data) {
+            this.documents = data.docs;
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          this.errors.push(error);
+          this.errored = true;
+          this.is_searching = false;
+        })
+        .finally(() => {
+          this.is_searching = false;
+        });
+
+      if (this.lda_model_id != "") {
+        options = {
+          corpus_id: this.corpus_id,
+          model_id: this.lda_model_id,
+          topn_topics: 10,
+          total_topic_score: 0.8,
+          clean_doc: true,
+        };
+
+        this.$http
+          .post("/api/lda_doc_topics?" + $.param(options), data, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          })
+          .then((response) => {
+            let data = response.data;
+            if ("topics" in data) {
+              this.uploaded_doc_topics = data.topics;
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+            this.errors.push(error);
+            this.errored = true;
+            this.is_searching = false;
+          })
+          .finally(() => {
+            this.is_searching = false;
+          });
+      }
+
+      e.preventDefault();
+    },
     getRelatedWords: function () {
       this.loading = true;
       this.$http
