820cf26e4d368aee9fe843f00776609b96c0d494
Aivin V. Solatorio <avsolatorio@gmail.com>
2020-11-25 06:38:57 +0000

    Add loading snippet.

--- scripts/cleaning/clean_corpus.py
+++ scripts/cleaning/clean_corpus.py
@@ -20,19 +20,21 @@ _logger = logging.getLogger(__name__)
 @click.command()
 @click.option('-c', '--config', 'cfg_path', required=True,
               type=click.Path(exists=True), help='path to yaml config file')
+@click.option('--input-dir', 'input_dir', required=True,
+              type=click.Path(exists=True), help='path to directory of raw text files')
 @click.option('--quiet', 'log_level', flag_value=logging.WARNING, default=True)
 @click.option('-v', '--verbose', 'log_level', flag_value=logging.INFO)
 @click.option('-vv', '--very-verbose', 'log_level', flag_value=logging.DEBUG)
 @click.version_option(wb_nlp.__version__)
-def main(cfg_path: Path, log_level: int):
+def main(cfg_path: Path, input_dir: Path, log_level: int):
     logging.basicConfig(stream=sys.stdout,
                         level=log_level,
                         datefmt='%Y-%m-%d %H:%M',
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     # YOUR CODE GOES HERE! Keep the main functionality in src/wb_nlp
-    # est = wb_nlp.models.Estimator()
-    with open(cfg_path) as fl:
-        config = yaml.safe_load(fl)
+
+    with open(cfg_path) as cfg_file:
+        config = yaml.safe_load(cfg_file)
 
     cleaner = wb_nlp.cleaning.cleaner.BaseCleaner(
         config=config,
@@ -42,8 +44,11 @@ def main(cfg_path: Path, log_level: int):
         max_token_length=config['max_token_length']
     )
 
-    for text in raw_text:
-        logging.info()
+    for fname in input_dir.glob('*.txt'):
+
+        logging.info(fname)
+        with open(fname) as file:
+            text = file.read()
         tokens = cleaner.get_clean_tokens(text)
 
 
