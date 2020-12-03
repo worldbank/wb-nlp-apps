7e1d2eef3eb791e121c48d0cc248a47f4d49be76
Aivin V. Solatorio <avsolatorio@gmail.com>
2020-12-03 06:42:36 +0000

    Upload script and config updates.

--- scripts/models/train_lda_model.py
+++ scripts/models/train_lda_model.py
@@ -8,10 +8,14 @@ import click
 from IPython.core import ultratb
 
 import wb_nlp
+from wb_nlp.configs.utils import load_config
 
-# fallback to debugger on error
-sys.excepthook = ultratb.FormattedTB(
-    mode='Verbose', color_scheme='Linux', call_pdb=1)
+import gensim
+from gensim.models.ldamulticore import LdaMulticore
+
+# # fallback to debugger on error
+# sys.excepthook = ultratb.FormattedTB(
+#     mode='Verbose', color_scheme='Linux', call_pdb=1)
 
 _logger = logging.getLogger(__file__)
 
@@ -31,7 +35,29 @@ def main(cfg_path: Path, log_level: int):
     # YOUR CODE GOES HERE! Keep the main functionality in src/wb_nlp
     # est = wb_nlp.models.Estimator()
 
-    _logger.info('Test')
+    config = load_config(cfg_path, 'model_config', _logger)
+
+    assert(gensim.__version__ == config['meta']['library_version'])
+
+    # Compute using gensim dictionary
+    id2word = {}
+    corpus = []
+
+    # Find parameters that are lists
+    params = config['params']
+    list_params = sorted(filter(lambda x: isinstance(params[x], list), params))
+    _logger.info(list_params)
+
+    params['id2word'] = id2word
+
+    params_set = []
+
+    for lp in list_params:
+        for v in params[lp]:
+            pass
+
+    lda = LdaMulticore(**params)
+    lda.update(corpus)
 
 
 if __name__ == '__main__':
