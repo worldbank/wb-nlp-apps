7bde70306351ee5ff324deb7e552a8c023d8a75b
Aivin V. Solatorio <avsolatorio@gmail.com>
2020-12-23 05:11:11 +0000

    Make content of the cleaning data comprehensive.

--- src/wb_nlp/types/cleaning.py
+++ src/wb_nlp/types/cleaning.py
@@ -1,79 +1,168 @@
-from wb_nlp.utils.data_types import HashableDict
-from wb_nlp.cleaning import cleaner
-from pydantic import BaseModel, Field
-from fastapi import APIRouter, Depends, HTTPException, Body
-from functools import lru_cache
-from typing import Optional, List
+'''
+This module contains the type definitions for the configuration parameters
+of the cleaning pipeline.
+
+This typed data structure will be used in the implementation of the API using
+FastAPI.
+'''
+
 import enum
 from typing import List, Any
-
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, validator
 from wb_nlp.utils.scripts import generate_model_hash
 
 
-class SpaCyPOSTag(enum.Enum):
+class SpaCyPOSTag(str, enum.Enum):
     '''Enum of SpaCy part-of-speech tags.
     '''
     # SpaCy pos tags
-    noun = "NOUN"
     adjective = "ADJ"
-    verb = "VERB"
-    propn = "PROPN"
+    adposition = "ADP"
     adverb = "ADV"
-    # 'ADP', 'AUX', 'CONJ', 'CCONJ', 'DET', 'INTJ',
-    # 'NUM', 'PART',
+    auxiliary = "AUX"
+    conjunction = "CONJ"
+    coordinating_conjunction = "CCONJ"
+    determiner = "DET"
+    interjection = "INTJ"
+    noun = "NOUN"
+    numeral = "NUM"
+    particle = "PART"
+    pronoun = "PRON"
+    proper_noun = "PROPN"
+    punctuation = "PUNCT"
+    subordinating_conjunction = "SCONJ"
+    symbol = "SYM"
+    verb = "VERB"
+    other = "X"
+    space = "SPACE"
+
+    class Config:
+        use_enum_values = True
 
 
-class Entity(enum.Enum):
+class Entity(str, enum.Enum):
     '''Enum of SpaCy entities.
     '''
-    cardinal = "CARDINAL"
-    time = "TIME"
-    percent = "PERCENT"
-    money = "MONEY"
-    date = "DATE"
-    quantity = "QUANTITY"
-    ordinal = "ORDINAL"
+    cardinal = "CARDINAL"           # Numerals that do not fall under another type.
+    time = "TIME"                   # Times smaller than a day.
+    percent = "PERCENT"             # Percentage, including ”%“.
+    money = "MONEY"                 # Monetary values, including unit.
+    date = "DATE"                   # Absolute or relative dates or periods.
+    quantity = "QUANTITY"           # Measurements, as of weight or distance.
+    ordinal = "ORDINAL"             # “first”, “second”, etc.
+    language = "LANGUAGE"           # Any named language.
+    law = "LAW"                     # Named documents made into laws.
+    work_of_art = "WORK_OF_ART"     # Titles of books, songs, etc.
+
+    # Named hurricanes, battles, wars, sports events, etc.
+    event = "EVENT"
+    # Objects, vehicles, foods, etc. (Not services.)
+    product = "PRODUCT"
+    # Non-GPE locations, mountain ranges, bodies of water.
+    loc = "LOC"
+    gpe = "GPE"                     # Countries, cities, states.
+    org = "ORG"                     # Companies, agencies, institutions, etc.
+    # Buildings, airports, highways, bridges, etc.
+    fac = "FAC"
+    # Nationalities or religious or political groups.
+    norp = "NORP"
+    person = "PERSON"               # People, including fictional.
+
+    class Config:
+        use_enum_values = True
 
 
 class LanguageFilter(BaseModel):
-    lang: str
-    score: float
+    """Data type for language detection.
+    """
+    lang: str = Field(
+        ...,
+        description="Language code as defined in enchant library.")
+    score: float = Field(
+        ...,
+        description="Threshold score for a detected language to be considered as significant."
+    )
+
+    def __gt__(self, other):
+        # Add this so that we could sort this class later.
+        return self.lang > other.lang
+
+    # def __eq__(self, other):
+    #     return self.lang == other.lang
+
+    def __lt__(self, other):
+        # Add this so that we could sort this class later.
+        return self.lang < other.lang
 
 
 class CleanerFlags(BaseModel):
-    expand_acronyms: bool = True
-    tag_whitelisted_entities: bool = True
+    """Container of flags that control the
+    behavior of the cleaning pipeline.
+    """
     correct_misspelling: bool = True
-    filter_stopwords: bool = True
-    fix_fragmented_tokens: bool = True
-    filter_language: bool = True
-    include_pos_tags: bool = True
     exclude_entity_types: bool = True
+    expand_acronyms: bool = True
+    filter_stopwords: bool = True
     filter_language: bool = True
+    fix_fragmented_tokens: bool = True
+    include_pos_tags: bool = True
+    tag_whitelisted_entities: bool = True
 
 
 class CleanerParams(BaseModel):
-    include_pos_tags__pos: List[SpaCyPOSTag] = []
-    exclude_entity_types__entities: List[Entity] = []
-    fix_fragmented_tokens__max_len: int = 5
-    filter_language__langs: List[LanguageFilter] = [
-        LanguageFilter(lang='en', score=0.98)]
+    '''Definition of parameters for the cleaner pipeline.
+    '''
+    pos_tags: List[SpaCyPOSTag] = Field(
+        ...,
+        description="List of SpaCy part-of-speech tags to be `included` in the cleaned text.")
+    entities: List[Entity] = Field(
+        ...,
+        description="List of SpaCy entity types to be `excluded` in the cleaned text.")
+    languages: List[LanguageFilter] = Field(
+        ...,
+        description="List of languages code defined in the `pyenchant` library that will be considered as valid.")
+    fragmented_token_max_len: int = Field(
+        5, description="Maximum number of tokens to consider for fragmented text fixing.")
+
+    @validator('entities')
+    def sort_entities(cls, v):
+        return sorted(v)
+
+    @validator('pos_tags')
+    def sort_pos_tags(cls, v):
+        return sorted(v)
+
+    @validator('languages')
+    def sort_languages(cls, v):
+        return sorted(v)
 
 
 class Cleaner(BaseModel):
+    """Main cleaner configuration body that contains the specification
+    for the flags and parameters that will be used in the cleaning process.
+    """
     config_id: str = ''
     flags: CleanerFlags = CleanerFlags(
         expand_acronyms=True,
-        tag_whitelisted_entities=True,
         correct_misspelling=True,
         filter_stopwords=True,
         fix_fragmented_tokens=True,
         filter_language=True,
         include_pos_tags=True,
         exclude_entity_types=True,
+        tag_whitelisted_entities=True,
+
     )
-    params: CleanerParams = CleanerParams()
+    params: CleanerParams = CleanerParams(
+        pos_tags=[SpaCyPOSTag.noun],
+        entities=[
+            Entity.cardinal,
+            Entity.money,
+            Entity.time,
+            Entity.percent],
+        languages=[LanguageFilter(
+            lang='en', score=0.98)]
+    )
     min_token_length: int = 3
     max_token_length: int = 50
 
@@ -88,127 +177,3 @@ class Cleaner(BaseModel):
         super().__init__(**temp_data)
 
         self.config_id = generate_model_hash(self.dict())
-
-
-'''This router contains the implementation for the cleaning API.
-'''
-
-
-class Params(BaseModel):
-    use: bool = True
-    params: dict = {}
-
-
-class RespellerInit(BaseModel):
-    dictionary_file: Optional[str] = None
-    spell_threshold: float = 0.25
-    allow_proper: bool = False  # Don't include proper names in suggestions
-    spell_cache: Optional[dict] = None
-
-
-class RespellerInferCorrectWord(BaseModel):
-    sim_thresh: float = 0.0
-    print_log: bool = False
-    min_len: int = 3
-    use_suggest_score: bool = True
-
-
-class RespellerInferCorrectWords(BaseModel):
-    return_tokens_as_list: bool = True
-
-
-class RespellerConfig(BaseModel):
-    init: RespellerInit
-    infer_correct_word: RespellerInferCorrectWord
-    infer_correct_words: RespellerInferCorrectWords
-
-
-class SpellCheckerInit(BaseModel):
-    lang: str = "en_US"
-    text: Optional[str] = None
-
-
-class SpellCheckerConfig(BaseModel):
-    init: SpellCheckerInit
-    # tokenize: Optional[str] = None
-    # chunkers: Optional[str] = None
-    # filters: Optional[str] = None
-
-
-# class FilterByPOSParams(Params):
-#     params:
-#     include_pos_tags: List
-
-
-class CleanerFixFragmentedTokensParams(BaseModel):
-    max_len: int = 5
-
-
-class CleanerFixFragmentedTokens(Params):
-    params: CleanerFixFragmentedTokensParams
-
-
-class CleanerFilterLanguageParams(BaseModel):
-    langs: List[str] = ["en"]
-    score: float = Field(
-        0.98, gt=0, description="Threshold for a detected language to be valid.")
-
-
-class CleanerFilterLanguage(Params):
-    params: CleanerFilterLanguageParams
-
-
-class CleanerConfig(BaseModel):
-    """This defines the data model for the cleaner config.
-    """
-    # Options for cleaning.corrector.recover_segmented_words
-    fix_fragmented_tokens: CleanerFixFragmentedTokens
-    # Expand the acronyms in the text
-    expand_acronyms: Params
-    # Update the spacy doc with the whitelisted entitiy tag
-    tag_whitelisted_entities: Params
-    # Use the part-of-speech as filter
-    filter_by_pos: Params
-    # Use extracted entities as filter
-    filter_by_entities: Params
-    # Check and fix spelling based on the Respeller module
-    correct_misspelling: Params
-    # Remove stopwords from the text
-    filter_stopwords: Params
-    # Filter by language
-    filter_language: CleanerFilterLanguage
-    include_pos_tags: List[POSTag] = [
-        POSTag.adjective.value, POSTag.noun.value]
-    exclude_entity_types: List[Entity] = [
-        Entity.cardinal.value, Entity.time.value]
-
-    # class Config:
-    #     schema_extra = {
-    #         "example": "Example"
-    #     }
-
-
-class POSTagType(BaseModel):
-    pos: POSTag
-
-
-class EntityType(BaseModel):
-    ent: Entity
-
-############# END: DEFINITION OF DATA TYPES AND MODELS #############
-
-
-@lru_cache(maxsize=32)
-def get_cleaner(config):
-    """This creates a cleaner instances and caches (LRU) the
-    object based on the config.
-    """
-    print('Getting cleaner instance...')
-
-    return cleaner.BaseCleaner(
-        config=config,
-        include_pos=config['include_pos_tags'],
-        exclude_entities=config['exclude_entity_types'],
-        min_token_length=config['min_token_length'],
-        max_token_length=config['max_token_length']
-    )
