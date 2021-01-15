43d2d760115f87d42ef9eea1e8405e7ffe03c64b
Aivin V. Solatorio <avsolatorio@gmail.com>
2021-01-15 04:00:20 +0000

    Create a doc count endpoint.

--- app/nlp_api/routers/metadata.py
+++ app/nlp_api/routers/metadata.py
@@ -1,5 +1,6 @@
 '''This router contains the implementation for the cleaning API.
 '''
+from bson.son import SON
 import enum
 from datetime import datetime
 from typing import Optional, List
@@ -19,7 +20,10 @@ router = APIRouter(
 )
 
 
-@ router.get("/get_metadata_by_id", response_model=metadata.MetadataModel)
+@ router.get(
+    "/get_metadata_by_id",
+    response_model=metadata.MetadataModel,
+    summary="Get metadata by id")
 def get_doc_metadata_by_id(id: str):
     """This enpoint fetches the metadata corresponding to the given `id`.
     """
@@ -27,6 +31,71 @@ def get_doc_metadata_by_id(id: str):
     doc = mongodb.get_docs_metadata_collection().find_one({"id": id})
     doc = metadata.MetadataModel(**doc)
 
-    # doc = MetadataModel(hex_id="1232", int_id=1231, corpus=corpus, filename_original="asff.txt", last_update_date=datetime.now(
-    # ), path_original="/asdad/wefsdc/asdasd/woiefsdf.txt", title="Hello world", doc_type=['Agreement'])
     return doc
+
+
+@router.post(
+    "/get_doc_count",
+    # response_model=metadata.MetadataModel,
+    summary="Get count of documents based on arbitrary grouping fields.")
+def get_doc_count(group_fields: List[str] = ["year", "country"], sort_field: dict = {"year": -1, "count": -1}, limit: int = 10):
+    """This endpoint provides a generic interface to get a summary count of documents given an arbitrary set of `group_fields`. The return value can be sorted based on the `sort_field` input. The number of returned groups is limited by the `limit` parameter.
+    """
+
+    assert len(set(sort_field).difference(group_fields + ['count'])) == 0
+
+    group_id = {b: f"${b}" for b in group_fields}
+
+    sort_field = {f"_id.{s}" if s !=
+                  "count" else s: v for s, v in sort_field.items()}
+    sort_field = SON(sort_field.items())
+    projection = {b: f"$_id.{b}" for b in group_fields}
+    projection["count"] = "$count"
+    projection["_id"] = 0
+
+    print(projection)
+
+    # Identify fields that needs unwinding, if any
+    list_fields = set(["adm_region", "author", "country", "der_acronyms", "doc_type",
+                       "geo_region", "major_doc_type", "topics_src", "wb_subtopic_src"])
+    unwind_fields = [{"$unwind": f"${b}"}
+                     for b in list_fields.intersection(group_fields)]
+
+    pipeline = []
+
+    if unwind_fields:
+        pipeline = unwind_fields
+
+    pipeline.extend([
+        {"$group": {"_id": group_id, "count": {"$sum": 1}}},
+        {"$sort": sort_field},
+        {"$limit": limit},
+        {"$project": projection},
+    ])
+
+    print(pipeline)
+
+    agg = mongodb.get_docs_metadata_collection().aggregate(
+        pipeline
+    )
+
+    values = list(agg)
+
+    return values
+
+
+# pipeline = [
+#     {"$unwind": "$country"},
+#     {"$group": {"_id": {"date_published": "$date_published",
+#                         "country": "$country"}, "count": {"$sum": 1}}},
+#     {"$sort": SON([("_id.date_published", -1), ("count", -1)])}]
+
+
+# pipeline = [
+#     {"$unwind": "$country"},
+#     {"$group": {"_id": {"year": "$year",
+#                         "country": "$country"}, "count": {"$sum": 1}}},
+#     {"$sort": SON([("_id.year", -1), ("count", -1)])},
+#     {"$project": {"_id": 0, "year": "$_id.year",
+#                   "country": "$_id.country", "count": "$count"}}
+# ]
