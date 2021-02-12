9ed5b49522a4ad8be5362ef7ca857a7b570ff2d4
Aivin V. Solatorio <avsolatorio@gmail.com>
2021-02-12 20:41:06 +0000

    Upload additional Corpus.vue static content.

--- app/app_kcp/src/components/pages/Corpus.vue
+++ app/app_kcp/src/components/pages/Corpus.vue
@@ -58,6 +58,41 @@
         a significant number of unknown words which could not be automatically
         corrected and created noise that had to be excluded.
       </p>
+      <h4>Languages</h4>
+      <p>
+        Our corpus is intended to be a corpus of documents in English. Some of
+        the scraped documents may be multi-lingual, and others may be in other
+        languages. Automatic language detection was applied to retain English
+        documents only. As our corpus is largely composed of documents from
+        international agencies or regional agencies from the United Nations
+        system, the selection of English documents is not considered as a major
+        issue for most of the purposes we pursue. We made an exception for
+        documents originating from the African Development Bank or Interamerican
+        Development Bank, for the needs of a project that requires a
+        comprehensive corpus of documents from multilateral development banks.
+        Documents written in French and Spanish have been translated using
+        Google / Amazon translation services. Although the translations may not
+        be perfect, they were found to be remarkably good and suitable for being
+        used in word embedding and topic models.
+      </p>
+      <h4>Exclusions</h4>
+      <p>
+        We have excluded documents that were found to have too little relevant
+        content. This includes documents that, after processing, contained less
+        than 250 tokens. We also excluded documents like procurement plans,
+        reports from administrative tribunals, budgets, and other administrative
+        documents. This exclusion is based on some heuristics and cannot be
+        guaranteed without errors of inclusion or exclusion.
+      </p>
+      <h4>Sources</h4>
+      <p>
+        Collect documents from selected sections of the organizations’ website.
+        Then run some procedure to exclude some documents (too short, not a
+        readable format, etc.). No interest in procurement plans, administrative
+        tribunal, budgets, … We cannot guarantee that all have been excluded,
+        and that no useful document has been missed. Exclude documents that are
+        not publicly available.
+      </p>
     </div>
   </div>
 </template>
