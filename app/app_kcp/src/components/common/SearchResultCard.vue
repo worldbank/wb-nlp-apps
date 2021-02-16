3efd3c61af73e7119f7a169ffc1c1ed32cd36103
Aivin V. Solatorio <avsolatorio@gmail.com>
2021-02-16 19:25:00 +0000

    Start implementing search feature.

---
+++ app/app_kcp/src/components/common/SearchResultCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="document-row" data-url="#" title="View study">
+      <div class="row">
+        <div class="col-3 col-lg-3">
+          <img
+            src="/static/files/doc_thumb.png"
+            title="document thumbnail"
+            alt="document thumbnail"
+          />
+        </div>
+        <div class="col-9 col-lg-9">
+          <span class="badge badge-primary wbg-badge">DOCUMENT</span>
+          <h5 class="title">
+            <a
+              href="https://mtt-wb21h.netlify.app/search/#"
+              title="Service Provision Assessment Survey 2018-2019"
+              >Service Provision Assessment Survey 2018-2019</a
+            >
+          </h5>
+          <div class="study-country">Afghanistan, 2018-2019</div>
+          <div class="sub-title">
+            <div>
+              <span class="study-by">Ministry of Public Health</span>
+            </div>
+            <div class="owner-collection">
+              Collection:
+              <a href="https://mtt-wb21h.netlify.app/search/#"
+                >MEASURE DHS: Demographic and Health Surveys</a
+              >
+            </div>
+          </div>
+          <div class="survey-stats">
+            <span>Created on: Mar 17, 2020</span>
+            <span>Last modified: Mar 17, 2020</span>
+            <span>Views: 3320</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/wb_nlp/interfaces/elasticsearch.py
@@ -0,0 +1,94 @@
+from datetime import datetime
+
+from pathlib import Path
+from elasticsearch_dsl import Document, Date, Integer, Keyword, Text
+from elasticsearch_dsl.connections import connections
+
+# from elasticsearch import helpers
+# from elasticsearch import Elasticsearch
+# es = Elasticsearch(hosts=[{"host": "es01", "port": 9200}])  # ,
+
+from elasticsearch_dsl import FacetedSearch, TermsFacet, DateHistogramFacet
+
+from wb_nlp.dir_manager import get_path_from_root
+
+connections.create_connection(hosts=['es01'])
+
+
+class NLPDoc(Document):
+    title = Text(analyzer='snowball', fields={'raw': Keyword()})
+    body = Text(analyzer='snowball')
+    author = Keyword()
+    country = Keyword()
+    corpus = Keyword()
+    date_published = Date()
+    last_update_date = Date()
+    year = Integer()
+    tokens = Integer()
+    views = Integer()
+
+    class Index:
+        name = 'nlp-documents'
+        settings = {
+            "number_of_shards": 2,
+            "number_of_replicas": 1,
+        }
+
+    def save(self, **kwargs):
+        self.tokens = len(self.body.split())
+        self.views = 0
+        return super(NLPDoc, self).save(**kwargs)
+
+
+NLPDoc.init()
+
+
+class NLPDocFacetedSearch(FacetedSearch):
+    # doc_types = [elasticsearch.NLPDoc, ]
+    doc_types = [NLPDoc, ]
+    # fields that should be searched
+    fields = ['country', 'title', 'body']
+
+    facets = {
+        # use bucket aggregations to define facets
+        'countries': TermsFacet(field='country'),
+        'publishing_frequency': DateHistogramFacet(field='date_published', interval='year')
+    }
+
+
+def get_connections():
+    return connections
+
+
+def make_nlp_docs_from_docs_metadata(docs_metadata):
+    # test_docs_metadata = mongodb.get_collection(
+    #     db_name="test_nlp", collection_name="docs_metadata")
+    # elasticsearch.make_nlp_docs_from_docs_metadata(test_docs_metadata.find({}))
+
+    root_path = Path(get_path_from_root())
+
+    for data in docs_metadata:
+        doc_path = root_path / data["path_original"]
+
+        # create and save and article
+        nlp_doc = NLPDoc(meta={'id': data["_id"]}, **data)
+
+        with open(doc_path, 'rb') as open_file:
+            doc = open_file.read().decode('utf-8', errors='ignore')
+            nlp_doc.body = doc
+
+        nlp_doc.save()
+
+
+def faceted_search_nlp_docs():
+    ns = NLPDocFacetedSearch()
+    response = ns.execute()
+
+    for hit in response:
+        print(hit.meta.score, hit.title)
+
+    for (country, count, selected) in response.facets.countries:
+        print(country, ' (SELECTED):' if selected else ':', count)
+
+    for (year, count, selected) in response.facets.publishing_frequency:
+        print(year.strftime('%Y'), ' (SELECTED):' if selected else ':', count)
