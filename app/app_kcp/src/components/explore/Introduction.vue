49f9d8fc57eb07e2661136b1544781b347a95f8b
Aivin V. Solatorio <avsolatorio@gmail.com>
2021-03-08 21:41:48 +0000

    Remove stuff.

--- app/app_kcp/src/components/explore/Introduction.vue
+++ app/app_kcp/src/components/explore/Introduction.vue
@@ -47,40 +47,12 @@
           >filtering by topic share</router-link
         >
         page explores a novel way of discovering documents simply by setting a
-        desired level of topic intensity.
+        desired level of topic intensity. Another application of a topic model
+        is that it allows us to analyze the evolution of topic popularity across
+        varying document types over time. This feature can be explored in the
+        <router-link to="/explore/topic-profiles">topic profiles</router-link>
+        page.
       </p>
-
-      <p>
-        Describes the sources and coverage of our corpus, provides interactive
-        visualizations of topic composition (extracted from LDA models), and
-        allows you to filter documents by their topic composition. Also provides
-        access to word embedding models, similarity measures, and others.
-      </p>
-
-      <br />
-      Explain purpose and main components of the “EXPLORE” section of the site.
-      <br />
-      <br />
-      <ul>
-        <li>
-          Corpus describes the collection of documents (sources, scope,
-          coverage, updates)
-        </li>
-        <li>
-          Topic composition based on LDA model; describes the distribution of
-          topics in the corpus or sub-corpus for different LDA models
-        </li>
-        <li>
-          Topic profiles; select one topic, and see how it has evolved over
-          time, by region, source, type, etc.
-        </li>
-        <li>Classification model: taxonomy tagging</li>
-        <li>Word embeddings: semantic closeness using word2vec</li>
-        <li>
-          Similarity: select or load a document, find closest based on different
-          measures of similarity
-        </li>
-      </ul>
 
       All code (except scrapers) in GitHub. See Methods and Tools.
     </div>
