d39221edea37b58f03b97a2688adf3094914b2ce
Aivin V. Solatorio <avsolatorio@gmail.com>
2021-03-18 18:11:47 +0000

    Some cleanup.

--- app/app_kcp/src/components/common/SearchResultCard.vue
+++ app/app_kcp/src/components/common/SearchResultCard.vue
@@ -12,9 +12,6 @@
         <div class="col-9 col-lg-9">
           <!-- <span class="badge badge-primary wbg-badge">DOCUMENT</span> -->
           <h5 class="title">
-            <!-- <a :href="result.url_pdf" :title="result.title">{{
-              result.title
-            }}</a> -->
             <router-link
               :to="{
                 name: 'document',
@@ -72,56 +69,6 @@
           </div>
         </div>
       </div>
-
-      <!-- <div class="row">
-        <div class="col-3 col-lg-3">
-          <img
-            src="/static/files/doc_thumb.png"
-            title="document thumbnail"
-            alt="document thumbnail"
-          />
-        </div>
-        <div class="col-9 col-lg-9"> -->
-      <!-- <span class="badge badge-primary wbg-badge">DOCUMENT</span> -->
-      <!-- <h5 class="title"> -->
-      <!-- <a :href="result.url_pdf" :title="result.title">{{
-              result.title
-            }}</a> -->
-      <!-- <router-link
-              :to="{
-                name: 'document',
-                params: { doc_id: result.id, metadata: result },
-              }"
-            > -->
-      <!-- <router-link
-              :to="{
-                path: '/document/' + result.id,
-                params: { doc_id: result.id, metadata: result },
-              }"
-            > -->
-      <!-- <router-link :to="document_link" :title="result.title"> -->
-      <!-- {{ result.title }}</router-link
-            >
-          </h5>
-          <div class="study-country">
-            {{ result.country[0] }}, {{ result.year }}
-          </div>
-          <div class="sub-title">
-            <div>
-              <span class="study-by">Ministry of Public Health</span>
-            </div>
-            <div class="owner-collection">
-              Corpus:
-              <router-link to="/search/">{{ result.corpus }}</router-link>
-            </div>
-          </div>
-          <div class="survey-stats">
-            <span>Created on: {{ getDate(result.date_published) }} </span>
-            <span>Last modified: {{ getDate(result.last_update_date) }} </span>
-            <span>Views: {{ result.views }}</span>
-          </div>
-        </div>
-      </div> -->
     </div>
     <a
       v-b-toggle
@@ -132,7 +79,6 @@
     >
     <b-collapse :id="result.id">
       <RelatedDocsPanel :reference_id="result.id" :submit="submit_related" />
-      <!-- :submit="submit_related" /> -->
     </b-collapse>
     <hr />
   </div>
