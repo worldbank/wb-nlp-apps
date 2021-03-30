f7d5f5477780bb073217a6fd05647035d9598ac0
Aivin V. Solatorio <avsolatorio@gmail.com>
2021-03-30 06:37:38 +0000

    Write outline for text preparation.

--- app/app_kcp/src/components/methods/TextPreparation.vue
+++ app/app_kcp/src/components/methods/TextPreparation.vue
@@ -2,14 +2,29 @@
   <div>
     <h1>{{ page_title }}</h1>
     <div>
+      This page discusses the details regarding the primary components of our
+      text processing pipeline. The following sections include technical
+      information about our selection of tools, techniques adopted and
+      developed, as well as some insights on our learnings while iterating on
+      the development of the pipeline.
+      <hr />
+      <h5>PDF processing</h5>
       <p>Conversion to text</p>
+      <h5>Metadata normalization</h5>
+      <p>This is the process of metadata normalization</p>
+      <h5>Stop words</h5>
       <p>Stop words (include CSV file)</p>
+      <h5>Phrase detection</h5>
       <p>Phrase detection</p>
+      <h5>Spell checking and vocabulary</h5>
       <p>Spell check</p>
+      <h5>Acronym extraction</h5>
       <p>Acronyms</p>
-      <p>n-grams (phrases)</p>
+      <h5>Named Entity Extraction</h5>
       <p>NER</p>
+      <h5>Extraction of country counts</h5>
       <p>Country counts (include CSV file)</p>
+      <h5>Language detection</h5>
       <p>Tokenization</p>
       <p>
         Simple interactive application showing before and after text cleaning of
