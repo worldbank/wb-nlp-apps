78b90dac517d928258ce86ff9e9bdbf3b8348ef0
Aivin V. Solatorio <avsolatorio@gmail.com>
2021-03-30 20:02:29 +0000

    Update wdi page with WB widgets.

--- app/app_kcp/src/components/common/SimilarWDIViewer.vue
+++ app/app_kcp/src/components/common/SimilarWDIViewer.vue
@@ -2,31 +2,33 @@
   <div v-if="results.length > 0">
     <div class="row wdi-group" v-for="result in results" :key="result.id">
       <div class="col-12">
-        <p class="lead">{{ result.name }}</p>
+        <p class="lead">
+          <a :href="result.url_wb" target="_blank">{{ result.name }}</a>
+        </p>
       </div>
 
-      <div class="col-4">
-        <a :href="result.url_data" target="_blank">Link to data</a>
+      <div class="col-6 mx-auto">
+        <iframe
+          class="wdi-frame"
+          v-if="result.url_wb"
+          :src="
+            'https://data.worldbank.org/share/widget?indicators=' +
+            getIndicatorName(result) +
+            '&view=map'
+          "
+          width="450"
+          height="300"
+          frameBorder="0"
+          scrolling="no"
+        ></iframe>
       </div>
-      <div class="col-4">
+
+      <div class="col-6">
+        <a :href="result.url_data" target="_blank">Link to data</a>
+        <br />
         <a :href="result.url_meta" target="_blank">Link to metadata</a>
       </div>
-
-      <div class="col-4">
-        <a :href="result.url_wb" target="_blank">Link to World Bank page</a>
-      </div>
-
-      <!-- <div class="col-1"></div>
-      <div class="col-11">{{ result.url_data }}</div>
-      <div class="col-1"></div>
-      <div class="col-11">{{ result.url_meta }}</div>
-      <div class="col-1"></div>
-      <div class="col-11">{{ result.url_wb }}</div> -->
     </div>
-    <!--
-    <ul v-if="results.length > 0">
-      <li v-for="result in results" :key="result.id">{{ result.name }}</li>
-    </ul> -->
   </div>
 </template>
 <script>
@@ -55,9 +57,17 @@ export default {
     return {
       results: [],
       loading: false,
+      indicator_name: null,
     };
   },
   methods: {
+    getIndicatorName(result) {
+      if (result.url_wb) {
+        var name = result.url_wb.split("/");
+        this.indicator_name = name[name.length - 1];
+        return this.indicator_name;
+      }
+    },
     getSimilarWDI() {
       this.loading = true;
       this.$http
@@ -79,6 +89,9 @@ export default {
 <style scoped>
 .wdi-group {
   margin-top: 20px !important;
-  margin-bottom: 50px !important;
+  margin-bottom: 75px !important;
+}
+.wdi-frame {
+  box-sizing: content-box;
 }
 </style>
