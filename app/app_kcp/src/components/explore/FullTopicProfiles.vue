62a88cf58ad707bba5f634d0ee837ecaf6b7c2c8
Aivin V. Solatorio <avsolatorio@gmail.com>
2021-05-02 04:07:34 +0000

    Add dataZoom and remove highlights.

--- app/app_kcp/src/components/explore/FullTopicProfiles.vue
+++ app/app_kcp/src/components/explore/FullTopicProfiles.vue
@@ -137,6 +137,10 @@ import {
   TooltipComponent,
   LegendComponent,
   GridComponent,
+  DataZoomComponent,
+  DataZoomInsideComponent,
+  DataZoomSliderComponent,
+  ToolboxComponent,
 } from "echarts/components";
 import { GraphChart, LinesChart, LineChart, BarChart } from "echarts/charts";
 import { CanvasRenderer } from "echarts/renderers";
@@ -150,6 +154,10 @@ use([
   LineChart,
   BarChart,
   GridComponent,
+  DataZoomComponent,
+  DataZoomInsideComponent,
+  DataZoomSliderComponent,
+  ToolboxComponent,
 ]);
 
 export default {
@@ -270,13 +278,19 @@ export default {
         },
         toolbox: {
           feature: {
+            dataZoom: {
+              yAxisIndex: "none",
+            },
+            restore: {},
             saveAsImage: {},
           },
         },
+
         grid: {
           left: "3%",
           right: "4%",
-          bottom: "3%",
+          bottom: "15%",
+          top: "10%",
           containLabel: true,
         },
         xAxis: [
@@ -291,6 +305,27 @@ export default {
             type: "value",
             axisLabel: {
               formatter: value === "share" ? "{value} %" : "{value}",
+            },
+          },
+        ],
+        dataZoom: [
+          {
+            type: "inside",
+            start: 50,
+            end: 100,
+          },
+          {
+            start: 50,
+            end: 100,
+            handleIcon:
+              "M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z",
+            handleSize: "50%",
+            handleStyle: {
+              color: "#fff",
+              shadowBlur: 3,
+              shadowColor: "rgba(0, 0, 0, 0.6)",
+              shadowOffsetX: 2,
+              shadowOffsetY: 2,
             },
           },
         ],
@@ -446,7 +481,7 @@ export default {
 }
 
 .chart {
-  height: 400px;
+  height: 450px;
 }
 
 .model-select-wrapper {
