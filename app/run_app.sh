1ef066bb9f4fd9aa01520030957549b3f3cf046c
Aivin V. Solatorio <avsolatorio@gmail.com>
2021-06-09 05:30:40 +0000

    Remove app/frontend web and php services.

--- app/run_app.sh
+++
@@ -1,2 +0,0 @@
-# gunicorn -w 4 -k gevent -b 0.0.0.0:8910 server:app
-gunicorn -w 4 --threads 4 --timeout 604800 --chdir /home/wb536061/wb-nlp/APP/ --log-level debug -b 0.0.0.0:8910 server:app &> /home/wb536061/wb-nlp/APP/app.log
