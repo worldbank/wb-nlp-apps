3c8768f6087ea1a7f848f41591c794901181cbfc
Aivin V. Solatorio <avsolatorio@gmail.com>
2021-06-14 02:57:24 +0000

    Implement function to update major document type.

--- src/wb_nlp/interfaces/elasticsearch.py
+++ src/wb_nlp/interfaces/elasticsearch.py
@@ -1038,3 +1038,35 @@ def get_topic_jdc_stats(model_run_info_id, topic_percentage):
     jdc_doc_topics = pd.DataFrame(jdc_doc_topics)
 
     return jdc_doc_stats.merge(jdc_doc_topics, on="id")
+
+
+###########################
+
+def set_other_wb_maj_doc_type_to_PR():
+    other_types = ["Evaluation Document",
+                   "Country Focus", "Economic and Sector Work"]
+    ids = get_ids_from_query(
+        NLPDoc,
+        query=NLPDoc.search().filter(
+            "terms", major_doc_type=other_types).to_dict()["query"],
+
+        ids_only=True
+    )
+    ids = list(ids)
+
+    if ids:
+        print(f"Updating major_doc_type for {len(ids)} documents...")
+
+        ubq = elasticsearch_dsl.UpdateByQuery(
+            index=NLPDoc.Index.name, using=get_client())
+        ubq = ubq.filter("terms", id=ids)
+        ubq = ubq.script(
+            source='ctx._source.major_doc_type=["Publications and Research"];')
+        ubq = ubq.params(scroll_size=50).params(
+            conflicts="proceed")  # .params(requests_per_second=10)
+
+        print("Executing major doc type update...")
+        response = ubq.execute()
+        print(response.to_dict())
+    else:
+        print("No ids that need updating...")
