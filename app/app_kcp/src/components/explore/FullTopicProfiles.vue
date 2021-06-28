7de85ecce84bcb55a55afd76590bba6c5cfbfb03
Aivin V. Solatorio <avsolatorio@gmail.com>
2021-06-28 22:04:27 +0000

    Fix FullTopicProfiles rendering.

--- app/app_kcp/src/components/explore/FullTopicProfiles.vue
+++ app/app_kcp/src/components/explore/FullTopicProfiles.vue
@@ -252,75 +252,10 @@ export default {
       return this.prev_topic_id != this.topic_id;
     },
     defaultOptions() {
-      return {
-        // title: {
-        //   text: "Topic profiles",
-        // },
-        tooltip: {
-          trigger: "axis",
-          axisPointer: {
-            type: "cross",
-            label: {
-              backgroundColor: "#6a7985",
-            },
-          },
-        },
-        legend: {
-          data: [],
-        },
-        toolbox: {
-          feature: {
-            // dataZoom: {
-            //   yAxisIndex: "none",
-            // },
-            // restore: {},
-            saveAsImage: {},
-          },
-        },
-        grid: {
-          left: "3%",
-          right: "4%",
-          bottom: "10%",
-          containLabel: true,
-        },
-        xAxis: [
-          {
-            type: "category",
-            boundaryGap: false,
-            data: [],
-          },
-        ],
-        yAxis: [
-          {
-            type: "value",
-            axisLabel: {
-              formatter: "{value}",
-            },
-          },
-        ],
-        dataZoom: [
-          {
-            type: "inside",
-            start: 20,
-            end: 100,
-          },
-          {
-            start: 20,
-            end: 100,
-            handleIcon:
-              "M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z",
-            handleSize: "50%",
-            handleStyle: {
-              color: "#fff",
-              shadowBlur: 3,
-              shadowColor: "rgba(0, 0, 0, 0.6)",
-              shadowOffsetX: 2,
-              shadowOffsetY: 2,
-            },
-          },
-        ],
-        series: [],
-      };
+      const value = "default";
+      const data = { default: { legend: null, year: null, series: null } };
+
+      return this.updateOption(data, value, "");
     },
   },
   mounted() {
@@ -361,7 +296,7 @@ export default {
           },
         },
         legend: {
-          data: data[value].legend,
+          data: data[value].legend || [],
         },
         toolbox: {
           feature: {
@@ -421,16 +356,40 @@ export default {
       console.log(label);
       return {
         // title: {
-        //   text: "Topic profiles" + "(" + label + ")",
+        //   text: "Topic profiles",
         // },
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "cross",
+            label: {
+              backgroundColor: "#6a7985",
+            },
+          },
+        },
         legend: {
-          data: data[value].legend,
+          data: data[value].legend || [],
+        },
+        toolbox: {
+          feature: {
+            // dataZoom: {
+            //   yAxisIndex: "none",
+            // },
+            // restore: {},
+            saveAsImage: {},
+          },
+        },
+        grid: {
+          left: "3%",
+          right: "4%",
+          bottom: "10%",
+          containLabel: true,
         },
         xAxis: [
           {
             type: "category",
             boundaryGap: false,
-            data: data[value].year,
+            data: data[value].year || [],
           },
         ],
         yAxis: [
@@ -441,7 +400,28 @@ export default {
             },
           },
         ],
-        series: data[value].series,
+        dataZoom: [
+          {
+            type: "inside",
+            start: 50,
+            end: 100,
+          },
+          {
+            start: 50,
+            end: 100,
+            handleIcon:
+              "M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z",
+            handleSize: "50%",
+            handleStyle: {
+              color: "#fff",
+              shadowBlur: 3,
+              shadowColor: "rgba(0, 0, 0, 0.6)",
+              shadowOffsetX: 2,
+              shadowOffsetY: 2,
+            },
+          },
+        ],
+        series: data[value].series || [],
       };
     },
     getModelTopics: function () {
